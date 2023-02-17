3592cdbe2984545d16f8a047853b3dfeb15866ee
amj-j <adam.janos24@gmail.com>
2023-02-18 00:43:03 +0100

    Added Difficuly button to MenuPanel

--- src/view/MenuPanel.java
+++ src/view/MenuPanel.java
@@ -6,22 +6,26 @@ import javax.swing.*;
 
 import interfaces.listeners.MenuListener;
 import interfaces.viewInterfaces.MenuView;
+import structures.MazeSize;
 
 public class MenuPanel extends JPanel implements MenuView {
     JLabel gamesWonLabel;
     JButton resetButton;
+    JButton mazeSizeButton;
 
     MenuListener listener;
 
     public MenuPanel() {     
-        //setBackground(DefaultSettings.BG_COLOR);
-        setBackground(new Color(255, 0, 0));
-        resetButton = new JButton("Reset");
+        setBackground(DefaultSettings.BG_COLOR);
         gamesWonLabel = new JLabel();
         gamesWonLabel.setBackground(DefaultSettings.BG_COLOR);
         gamesWonLabel.setText("Games won: 0");
-        add(resetButton);
+        resetButton = new JButton("Reset");
+        mazeSizeButton = new JButton("Difficulty");
         add(gamesWonLabel);
+        add(resetButton);
+        add(mazeSizeButton);
+        initPopupMenu();
         addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
@@ -44,5 +48,35 @@ public class MenuPanel extends JPanel implements MenuView {
 
     public void addListener(MenuListener listener) {
         this.listener = listener;
+    }
+
+    public void initPopupMenu() {
+        JPopupMenu popupMenu = new JPopupMenu();
+        JMenuItem easy = new JMenuItem("Easy");
+        JMenuItem medium = new JMenuItem("Medium");
+        JMenuItem hard = new JMenuItem("Hard");
+
+        ActionListener menuItemListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                if (event.getSource() == easy) { listener.changeMazeSize(new MazeSize(10, 10)); }
+                else if (event.getSource() == medium) { listener.changeMazeSize(new MazeSize(20, 15)); }
+                else if (event.getSource() == hard) { listener.changeMazeSize(new MazeSize(30, 20)); }
+            }
+        };
+
+        easy.addActionListener(menuItemListener);
+        medium.addActionListener(menuItemListener);
+        hard.addActionListener(menuItemListener);
+
+        popupMenu.add(easy);
+        popupMenu.add(medium);
+        popupMenu.add(hard);
+        mazeSizeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                popupMenu.show(mazeSizeButton, mazeSizeButton.getWidth(), 0);
+            }
+        });
     }
 }
